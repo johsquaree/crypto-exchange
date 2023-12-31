c357b0479d7606b90fa4bc54b8bbd65b032270a4
furkan-kayapinar <johsquaree@gmail.com>
2023-12-31 22:41:48 +0300

    book-created

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/johsquaree/crypto-exchange
+
+go 1.18
+
+require github.com/sirupsen/logrus v1.9.0
+
+require (
+	github.com/stretchr/testify v1.7.2 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
---
+++ orderbook/book.go
@@ -0,0 +1,344 @@
+package orderbook
+
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Trade struct {
+	Price     float64
+	Size      float64
+	Bid       bool
+	Timestamp int64
+}
+
+type Match struct {
+	Ask        *Order
+	Bid        *Order
+	SizeFilled float64
+	Price      float64
+}
+
+type Order struct {
+	ID        int64
+	UserID    int64
+	Size      float64
+	Bid       bool
+	Limit     *Limit
+	Timestamp int64
+}
+
+type Orders []*Order
+
+func (o Orders) Len() int           { return len(o) }
+func (o Orders) Swap(i, j int)      { o[i], o[j] = o[j], o[i] }
+func (o Orders) Less(i, j int) bool { return o[i].Timestamp < o[j].Timestamp }
+
+func NewOrder(bid bool, size float64, userID int64) *Order {
+	return &Order{
+		UserID:    userID,
+		ID:        int64(rand.Intn(10000000)),
+		Size:      size,
+		Bid:       bid,
+		Timestamp: time.Now().UnixNano(),
+	}
+}
+
+func (o *Order) String() string {
+	return fmt.Sprintf("[size: %.2f] | [id: %d]", o.Size, o.ID)
+}
+
+func (o *Order) Type() string {
+	if o.Bid {
+		return "BID"
+	}
+	return "ASK"
+}
+
+func (o *Order) IsFilled() bool {
+	return o.Size == 0.0
+}
+
+type Limit struct {
+	Price       float64
+	Orders      Orders
+	TotalVolume float64
+}
+
+type Limits []*Limit
+
+type ByBestAsk struct{ Limits }
+
+func (a ByBestAsk) Len() int           { return len(a.Limits) }
+func (a ByBestAsk) Swap(i, j int)      { a.Limits[i], a.Limits[j] = a.Limits[j], a.Limits[i] }
+func (a ByBestAsk) Less(i, j int) bool { return a.Limits[i].Price < a.Limits[j].Price }
+
+type ByBestBid struct{ Limits }
+
+func (b ByBestBid) Len() int           { return len(b.Limits) }
+func (b ByBestBid) Swap(i, j int)      { b.Limits[i], b.Limits[j] = b.Limits[j], b.Limits[i] }
+func (b ByBestBid) Less(i, j int) bool { return b.Limits[i].Price > b.Limits[j].Price }
+
+func NewLimit(price float64) *Limit {
+	return &Limit{
+		Price:  price,
+		Orders: []*Order{},
+	}
+}
+
+func (l *Limit) AddOrder(o *Order) {
+	o.Limit = l
+	l.Orders = append(l.Orders, o)
+	l.TotalVolume += o.Size
+}
+
+func (l *Limit) DeleteOrder(o *Order) {
+	for i := 0; i < len(l.Orders); i++ {
+		if l.Orders[i] == o {
+			l.Orders[i] = l.Orders[len(l.Orders)-1]
+			l.Orders = l.Orders[:len(l.Orders)-1]
+		}
+	}
+
+	o.Limit = nil
+	l.TotalVolume -= o.Size
+
+	sort.Sort(l.Orders)
+}
+
+func (l *Limit) Fill(o *Order) []Match {
+	var (
+		matches        []Match
+		ordersToDelete []*Order
+	)
+
+	for _, order := range l.Orders {
+		if o.IsFilled() {
+			break
+		}
+
+		match := l.fillOrder(order, o)
+		matches = append(matches, match)
+
+		l.TotalVolume -= match.SizeFilled
+
+		if order.IsFilled() {
+			ordersToDelete = append(ordersToDelete, order)
+		}
+	}
+
+	for _, order := range ordersToDelete {
+		l.DeleteOrder(order)
+	}
+
+	return matches
+}
+
+func (l *Limit) fillOrder(a, b *Order) Match {
+	var (
+		bid        *Order
+		ask        *Order
+		sizeFilled float64
+	)
+
+	if a.Bid {
+		bid = a
+		ask = b
+	} else {
+		bid = b
+		ask = a
+	}
+
+	if a.Size >= b.Size {
+		a.Size -= b.Size
+		sizeFilled = b.Size
+		b.Size = 0.0
+	} else {
+		b.Size -= a.Size
+		sizeFilled = a.Size
+		a.Size = 0.0
+	}
+
+	return Match{
+		Bid:        bid,
+		Ask:        ask,
+		SizeFilled: sizeFilled,
+		Price:      l.Price,
+	}
+}
+
+type Orderbook struct {
+	asks []*Limit
+	bids []*Limit
+
+	Trades []*Trade
+
+	mu        sync.RWMutex
+	AskLimits map[float64]*Limit
+	BidLimits map[float64]*Limit
+	Orders    map[int64]*Order
+}
+
+func NewOrderbook() *Orderbook {
+	return &Orderbook{
+		asks:      []*Limit{},
+		bids:      []*Limit{},
+		Trades:    []*Trade{},
+		AskLimits: make(map[float64]*Limit),
+		BidLimits: make(map[float64]*Limit),
+		Orders:    make(map[int64]*Order),
+	}
+}
+
+func (ob *Orderbook) PlaceMarketOrder(o *Order) []Match {
+	ob.mu.Lock()
+	defer ob.mu.Unlock()
+
+	matches := []Match{}
+
+	if o.Bid {
+		if o.Size > ob.AskTotalVolume() {
+			panic(fmt.Errorf("not enough volume [size: %.2f] for market order [size: %.2f]", ob.AskTotalVolume(), o.Size))
+		}
+
+		for _, limit := range ob.Asks() {
+			limitMatches := limit.Fill(o)
+			matches = append(matches, limitMatches...)
+
+			if len(limit.Orders) == 0 {
+				ob.clearLimit(false, limit)
+			}
+		}
+	} else {
+		if o.Size > ob.BidTotalVolume() {
+			panic(fmt.Errorf("not enough volume [size: %.2f] for market order [size: %.2f]", ob.BidTotalVolume(), o.Size))
+		}
+
+		for _, limit := range ob.Bids() {
+			limitMatches := limit.Fill(o)
+			matches = append(matches, limitMatches...)
+
+			if len(limit.Orders) == 0 {
+				ob.clearLimit(true, limit)
+			}
+		}
+	}
+
+	for _, match := range matches {
+		trade := &Trade{
+			Price:     match.Price,
+			Size:      match.SizeFilled,
+			Timestamp: time.Now().UnixNano(),
+			Bid:       o.Bid,
+		}
+		ob.Trades = append(ob.Trades, trade)
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"currentPrice": ob.Trades[len(ob.Trades)-1].Price,
+	}).Info()
+
+	return matches
+}
+
+func (ob *Orderbook) PlaceLimitOrder(price float64, o *Order) {
+	var limit *Limit
+
+	ob.mu.Lock()
+	defer ob.mu.Unlock()
+
+	if o.Bid {
+		limit = ob.BidLimits[price]
+	} else {
+		limit = ob.AskLimits[price]
+	}
+
+	if limit == nil {
+		limit = NewLimit(price)
+
+		if o.Bid {
+			ob.bids = append(ob.bids, limit)
+			ob.BidLimits[price] = limit
+		} else {
+			ob.asks = append(ob.asks, limit)
+			ob.AskLimits[price] = limit
+		}
+	}
+
+	logrus.WithFields(logrus.Fields{
+		"price":  limit.Price,
+		"type":   o.Type(),
+		"size":   o.Size,
+		"userID": o.UserID,
+	}).Info("new limit order")
+
+	ob.Orders[o.ID] = o
+	limit.AddOrder(o)
+}
+
+func (ob *Orderbook) clearLimit(bid bool, l *Limit) {
+	if bid {
+		delete(ob.BidLimits, l.Price)
+		for i := 0; i < len(ob.bids); i++ {
+			if ob.bids[i] == l {
+				ob.bids[i] = ob.bids[len(ob.bids)-1]
+				ob.bids = ob.bids[:len(ob.bids)-1]
+			}
+		}
+	} else {
+		delete(ob.AskLimits, l.Price)
+		for i := 0; i < len(ob.asks); i++ {
+			if ob.asks[i] == l {
+				ob.asks[i] = ob.asks[len(ob.asks)-1]
+				ob.asks = ob.asks[:len(ob.asks)-1]
+			}
+		}
+	}
+
+	fmt.Printf("clearing limit price level [%.2f]\n", l.Price)
+}
+
+func (ob *Orderbook) CancelOrder(o *Order) {
+	limit := o.Limit
+	limit.DeleteOrder(o)
+	delete(ob.Orders, o.ID)
+
+	if len(limit.Orders) == 0 {
+		ob.clearLimit(o.Bid, limit)
+	}
+}
+
+func (ob *Orderbook) BidTotalVolume() float64 {
+	totalVolume := 0.0
+
+	for i := 0; i < len(ob.bids); i++ {
+		totalVolume += ob.bids[i].TotalVolume
+	}
+
+	return totalVolume
+}
+
+func (ob *Orderbook) AskTotalVolume() float64 {
+	totalVolume := 0.0
+
+	for i := 0; i < len(ob.asks); i++ {
+		totalVolume += ob.asks[i].TotalVolume
+	}
+
+	return totalVolume
+}
+
+func (ob *Orderbook) Asks() []*Limit {
+	sort.Sort(ByBestAsk{ob.asks})
+	return ob.asks
+}
+
+func (ob *Orderbook) Bids() []*Limit {
+	sort.Sort(ByBestBid{ob.bids})
+	return ob.bids
+}
